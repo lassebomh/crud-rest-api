0fe5c77ce8c6e5628a34517d0aac2262ebb9aa79
Lasse Holmgaard Bomholt <lasse@bomh.net>
2020-05-08 07:54:39 +0200

    Fixed traverse function

--- server.js
+++ server.js
@@ -14,48 +14,60 @@ async function responses(props) {
     props.res.setHeader('Set-Cookie', 'session_id=42069420; path=/')
 
     return {
-        ":article_id": async (props) => {
-            props.res.writeHead(200)
-            props.res.write(`The article id is: ${props.article_id}. Amazing!`)
+        ":article_id": {
+            'comment': {
+                ':comment_id': async (props) => {
+                    props.res.writeHead(200)
+                    props.res.write(`Article ${props.article_id}. Comment ${props.comment_id}`)
+                }
+            }
         }
     }
 }
 
 async function traverse(terrain, map, inputs) {
-    location = terrain[map[0]]
-    if (location === undefined) {
-        for (let i = 0; i < Object.keys(terrain).length; i++) {
-            indexed = Object.keys(terrain)[i]
-            if (Object.keys(terrain)[i][0] === ":") {
-                location = terrain[indexed]
-                inputs[indexed.slice(1)] = map[0]
-                break
+
+    terrain = typeof terrain === "function" ? await terrain(inputs) : terrain
+    
+    if (typeof terrain === "object") {
+        let findResult = terrain[map[0]]
+        if (findResult === undefined) {
+            for (let i = 0; i < Object.keys(terrain).length; i++) {
+                indexed = Object.keys(terrain)[i]
+                if (Object.keys(terrain)[i][0] === ":") {
+                    findResult = terrain[indexed]
+                    inputs[indexed.slice(1)] = map[0]
+                    break
+                }
             }
         }
+        // console.log(terrain, map);
+        await traverse(findResult, map.slice(1), inputs)
+        
+    } else if (terrain === undefined) {
+        // console.log(terrain, map);
+        
     }
-    let nextTerrain;
-
-    
-
-    if (typeof location === 'function') {
-        let result = await location(inputs)
-        if (typeof result === 'object') {
-            nextTerrain = result
-        }
-    } else {
-        nextTerrain = location
-    }
-
-    if (nextTerrain != null) traverse(nextTerrain, map.slice(1), inputs)
-    // console.log(nextTerrain);
-    
 }
 
 http.createServer(async (req, res) => {
 
     let reqLocation = req.url.match(/\/((\/?[\w]+)*)\??.*/)[1].split('/')   
+    
+    try {
+        await traverse(responses, reqLocation, new Props(req, res, 420))
+    } catch(e) {
+        let error = [500]
+        if (e instanceof Error) {
+            console.error(e)
+        } else {
+            error = Number.isInteger(e) ? [e] : e
+            console.error(req.url+" "+error[0]+(error[1] ? " "+error[1] : "" ))
+        }
+        res.writeHead(...error)
+        res.end()
+    }
 
-    await traverse(responses, reqLocation, new Props(req, res, 420))
     res.end()
 
 }).listen(5000);
